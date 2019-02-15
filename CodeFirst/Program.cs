81663ffb9314ccf1633034573448fe49211429fa
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-02-15 12:25:13 +0800

    modifying existing class - adding a new property

--- CodeFirst/Program.cs
+++ CodeFirst/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace CodeFirst
@@ -8,6 +9,7 @@ namespace CodeFirst
         public string Title { get; set; }
         public string Description { get; set; }
         public Category Category { get; set; }
+        public DateTime? DatePublished { get; set; }
         public CourseLevel Level { get; set; }
         public float FullPrice { get; set; }
         public Author Author { get; set; }
