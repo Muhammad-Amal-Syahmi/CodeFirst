595cf4a92d331315723a3050fe5e9ceecd1f54e2
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-02-15 11:59:36 +0800

    Code-First with a new database

--- CodeFirst/Program.cs
+++ CodeFirst/Program.cs
@@ -1,21 +1,33 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace CodeFirst
 {
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; }
+    }
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { set; get; }
+        public DbSet<Tag> Tags { set; get; }
+        public PlutoContext()
+            : base("name= DefaultConnection")
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
