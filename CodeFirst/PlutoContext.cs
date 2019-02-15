dec1ad99a51e9cb4e96ca6079a65de81ee2c23af
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-02-15 12:59:55 +0800

    Deleting existing class (or table), and transfer data new class (or table)

--- CodeFirst/PlutoContext.cs
+++ CodeFirst/PlutoContext.cs
@@ -7,7 +7,6 @@ namespace CodeFirst
         public DbSet<Course> Courses { get; set; }
         public DbSet<Author> Authors { set; get; }
         public DbSet<Tag> Tags { set; get; }
-        public DbSet<Category> Categories { get; set; }
         public PlutoContext()
             : base("name= DefaultConnection")
         {
--- CodeFirst/Program.cs
+++ CodeFirst/Program.cs
@@ -8,7 +8,6 @@ namespace CodeFirst
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Category Category { get; set; }
         public CourseLevel Level { get; set; }
         public float FullPrice { get; set; }
         public Author Author { get; set; }
